d0902dfb65ee078b12c109837c7e4e1cee87b733
ikuhasdf <ikuhasdf@outlook.com>
2025-08-09 10:43:06 +0800

    Update 10.ino

--- 10.ino
+++ 10.ino
@@ -3,24 +3,40 @@ int y = 3;
 int g = 6;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(r, OUTPUT);
   pinMode(y, OUTPUT);
   pinMode(g, OUTPUT);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(r, HIGH);
-  delay(5000);
-  digitalWrite(r,LOW);
-  digitalWrite(g, HIGH);
-  delay(5000);
-  digitalWrite(g,LOW);
-  for (int i = 0; i < 4; i++)
+  int on_1 = digitalRead(8);
+  int off_1 = digitalRead(10);
+  if (on_1 == LOW)
   {
-    digitalWrite(y, HIGH);
-    delay(1000);
-    digitalWrite(y, LOW);
-    delay(1000);
+    digitalWrite(r, HIGH);
+    delay(5000);
+    digitalWrite(r, LOW);
+    digitalWrite(g, HIGH);
+    delay(5000);
+    digitalWrite(g, LOW);
+    for (int i = 0; i < 3; i++)
+    {
+      digitalWrite(y, HIGH);
+      delay(500);
+      digitalWrite(y, LOW);
+      delay(500);
+    }
   }
+  else if (off_1 == LOW)
+  {
+    digitalWrite(r, LOW);
+    digitalWrite(y, LOW);
+    digitalWrite(g, LOW);
+    while (digitalRead(10) == LOW) delay(1);
+  }
+  delay(100);
 }
